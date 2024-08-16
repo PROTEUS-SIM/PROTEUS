368829bac2394fedf9a4e6e5a2455d16073f3adc
Blanken <n.blanken@utwente.nl>
2024-08-16 18:48:00 +0200

    Compensate for the k-Wave staggered grid in transducer definition

--- acoustic-module/define_source_transducer.m
+++ acoustic-module/define_source_transducer.m
@@ -13,10 +13,27 @@ function source = define_source_transducer(Transducer, Transmit, ...
 % output:   source                  the transducer source object
 % =========================================================================
 
-% For previous versions, dipole sources were used for the transducer
-% definition (soft baffle).
-if ~isfield(Transducer,'SourceType')
-    Transducer.SourceType = 'dipole';
+p = Transmit.PressureSignal;
+M = length(p);
+
+% If Transducer.SourceType is defined, compensate for the effect of the
+% staggered k-Wave grid. See Table 2.1 on p. 14 of the k-Wave manual
+% (Manual Version 1.1, August 27, 2016). 
+% http://www.k-wave.org/manual/k-wave_user_manual_1.1.pdf
+if isfield(Transducer,'SourceType') && ...
+        strcmp(Transducer.SourceType,'dipole')
+    
+    Transducer.integration_points(:,:,1) = ...
+        Transducer.integration_points(:,:,1) - Grid.dx/2;
+
+    [source_weights, source_mask_idx] = ...
+        recompute_source_weights(Transducer,Grid);
+end
+
+if isfield(Transducer,'SourceType') && ...
+        strcmp(Transducer.SourceType,'monopole')
+    t = (0:(M-1))*Grid.dt;
+    p = sinc_interpolation(t, p, t+Grid.dt/2);
 end
 
 delays  = Transducer.integration_transmit_delays;
@@ -47,11 +64,10 @@ weights = weights(:);
 Z = Medium.SpeedOfSound*Medium.Density;
 
 % Compute signal length required to apply the delays:
-M = length(Transmit.PressureSignal);
 N = M + ceil(max(delays)/Grid.dt);
 
 % Apply delay and apodization.
-pressure_source = Transmit.PressureSignal.*apod;
+pressure_source = p.*apod;
 
 % Set up frequency axis (Hz)
 f = (0:(N-1))/(N*Grid.dt);
@@ -63,6 +79,12 @@ f(:,ceil(N/2+1):N) = -(f(:,floor(1+N/2):-1:2));
 pressure_source = fft(pressure_source,N,2);
 pressure_source = pressure_source.*exp(-2*pi*1i*delays*f);
 pressure_source = ifft(pressure_source,[],2,'symmetric');
+
+% For previous versions, dipole sources were used for the transducer
+% definition (soft baffle).
+if ~isfield(Transducer,'SourceType')
+    Transducer.SourceType = 'dipole';
+end
 
 if strcmp(Transducer.SourceType,'dipole')
     source.u_mask = zeros(Grid.Nx, Grid.Ny, Grid.Nz, 'logical');
@@ -79,5 +101,15 @@ else
     % of point sources.
     source.p = source_weights * (pressure_source.*weights);
 end
+
+end
+
+function [source_weights, source_mask_idx] = ...
+    recompute_source_weights(Transducer,Grid)
+
+[sensor, sensor_weights] = define_sensor_transducer(Transducer, Grid);
+
+source_weights  = transpose(sensor_weights);
+source_mask_idx = find(logical(sensor.mask));
 
 end
