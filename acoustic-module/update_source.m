6a863b473752a8eb3fd5a3c6efc2a4fef4a6e63b
= <nathanblanken@users.noreply.github.com>
2025-08-02 23:17:52 +0200

    Allow updating source with existing pressure mask
    The function update_source.m now adds new pressure source data to existing pressure source data, instead of overwriting existing data. This change enables the use of pressure (monopole) source transducers in addition to velocity (dipole) source transducers.

--- acoustic-module/update_source.m
+++ acoustic-module/update_source.m
@@ -1,5 +1,5 @@
 function source = update_source(...
-    source, mass_source, source_weights, source_mask_idx, Grid, medium)
+    source, mass_source, source_weights, source_mask_idx_new, Grid, medium)
 % Multiply the spatial delta function with the mass source signals of 
 % point source, convert to pressure signals and add to the source
 % struct.
@@ -10,19 +10,17 @@ function source = update_source(...
 % code kspaceFirstOrder_scaleSourceTerms.m
 
 % k-Wave cannot handle source masks with all values zero:
-if isempty(source_mask_idx)
+if isempty(source_mask_idx_new)
     return
 end
 
-% In a future version, we will solve the case in which the transducer is
-% also defined as a pressure source. Currently, only velocity source
-% transducers are supported.
+% Get source mask and source data of the original source:
 if isfield(source,'p_mask')
-    error('Input argument SOURCE already has a field P_MASK.')
+    p_old  = source.p;
+else
+    source.p_mask = zeros(Grid.Nx, Grid.Ny, Grid.Nz);
+    p_old  = [];
 end
-
-source.p_mask = zeros(Grid.Nx, Grid.Ny, Grid.Nz);
-source.p_mask(source_mask_idx) = 1;
 
 % Convert mass source to mass source density.
 % (From the k-Wave manual: S_M is a mass source term and represents the 
@@ -30,9 +28,27 @@ source.p_mask(source_mask_idx) = 1;
 mass_source = double(mass_source/(Grid.dx*Grid.dy*Grid.dz));
 
 % Speed of sound at the source points:
-c0 = medium.sound_speed(source_mask_idx);
+c0 = medium.sound_speed(source_mask_idx_new);
 
-source.p = (source_weights * mass_source) .* (c0*Grid.dx/2);
-source.p = cast(full(source.p),class(mass_source));
+p_new = (source_weights * mass_source) .* (c0*Grid.dx/2);
+p_new = cast(full(p_new),class(mass_source));
+
+% Add new source points to the source mask:
+source_mask_idx_old = find(source.p_mask);
+source.p_mask(source_mask_idx_new) = 1;
+source_mask_idx_full = find(source.p_mask);
+
+% Initialize output source data:
+N = length(source_mask_idx_full);
+Nt1 = size(p_old,2);
+Nt2 = size(p_new,2);
+source.p = zeros(N,max(Nt1,Nt2));
+
+% Add new source data to the original source data:
+[~,j_old] = ismember(source_mask_idx_old,source_mask_idx_full);
+[~,j_new] = ismember(source_mask_idx_new,source_mask_idx_full);
+
+source.p(j_old,1:Nt1) = p_old;
+source.p(j_new,1:Nt2) = source.p(j_new,1:Nt2) + p_new;
 
 end
